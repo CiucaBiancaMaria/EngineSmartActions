f8963f3fd3d82c23591b5e60b5721cbba176cd48
CiucaBiancaMaria <ciuca.bianca@yahoo.com>
2022-09-27 10:13:03 +0300

    adding record type consideration

--- src/orderValidation.js
+++ src/orderValidation.js
@@ -4,13 +4,14 @@ function runAction(payload) {
 		const [account] = related.Account;
 		const isProofOfDeliveryRequired = account.ProofOfDeliveryRequired__c;
 		const poNumber = order.PoNumber;
+		const recordType = order.RecordTypeId;
 		if (related.Account === undefined) {
 			throw new Error(`This order is not associated with an account.`);
 		}
 		if (isProofOfDeliveryRequired === undefined) {
 			throw new Error(`No access to ProofOfDeliveryRequired__c`);
 		}
-		if (isProofOfDeliveryRequired && !poNumber) {
+		if (isProofOfDeliveryRequired && !poNumber && recordType == "EDI Order") {
 			throw new Error(`Please fill out the Purchase Order number before completing the order!`);
 		}
 		payload.data.message = `Order Validated`;
